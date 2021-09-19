01f0b6271e986a23301d68f62f0fdd32e42e094d
Adriana <adriana.calvomatos@gmail.com>
2021-09-19 19:01:37 +0000

    add new line to last character

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,16 @@
 const sentence = 'hello there from lighthouse labs';
+// instead of console.log so each char does NOT print on a new line
 
 let delay = 0;
 
-for (const char of sentence) {
+for (let i = 0; i <= sentence.length - 1; i++) {
   setTimeout(() => {
-    // instead of console.log so each char does NOT print on a new line
-    process.stdout.write(char);
+    if (i === sentence.length - 1) {
+      // add new line after last character
+      process.stdout.write(sentence[i] + '\n');
+    } else {
+      process.stdout.write(sentence[i]);
+    }
   }, delay);
 
   delay += 50;
